d2060f5f9c61d25e6dbbc817a77742186025cc7b
Ivan Tishchenko <moodminds@hotmail.com>
2024-06-03 19:59:35 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# routes-traverse
-Routes Traverse - Synchronous Traverse implementation of the Routes SPI
+# Synchronous Traverse implementation of the [Routes](https://github.com/MoodMinds/routes) SPI
+
+This implementation of the [Routes SPI](https://github.com/MoodMinds/routes) offers support for synchronous execution traversal
+and utilizes [Traverse Streams Traversable](https://github.com/MoodMinds/traverse-streams-traversable) for this. It serves
+as an ideal extension-base implementation of the [Routes SPI](https://github.com/MoodMinds/routes) or as a client library
+when certain logic cannot be adequately expressed using the Java 8 Streams API. It excels in scenarios where you need to allow
+declaring checked exceptions in functions, implement exception handling, conditionals, and especially when you seek graceful,
+reusable stream definitions.
+
+## Understanding the Core
+
+The materialized execution representation of this [Routes](https://github.com/MoodMinds/routes) implementation is `Traversable`.
+
+```java
+import org.moodminds.route.Stream;
+import org.moodminds.route.Stream2;
+import org.moodminds.route.traverse.Routes;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import static java.lang.System.getenv;
+
+class Sample {
+
+    static Logger LOG = Logger.getLogger(Sample.class.getName());
+
+    static final Stream<String, RuntimeException> INTEGERS = $ -> $.source("1", "2", "3");
+
+    static final Stream2<Boolean, String, String, Exception> STREAMING = ($, bool, str) -> $
+            .either(bool, () -> $.source("a", "b", "c"))
+            .option(str, String::isEmpty, INTEGERS)
+            .option(str, "mm"::equals, () -> $
+                .supply(() -> getenv("VAR1")))
+            .option(str, "WW"::equals, () -> $
+                .supply(() -> {
+                    throw new IOException("Something went wrong");
+                }))
+            .option(() -> $
+                .expect(str));
+
+    // refer to STREAMING emitting Exception inside
+    static final Stream2<Boolean, String, String, RuntimeException> CATCHING = ($, bool, str) -> $
+            .stream(STREAMING, bool, str)
+            .caught(ex -> $
+                .supply(ex, Exception::getMessage))
+            .caught(IOException.class, ioEx -> $
+                .supply(ioEx, RuntimeException::new, $::except))
+            .caught(IllegalStateException.class, sEx -> $
+                .stream(INTEGERS))
+            .caught(RuntimeException.class, $::except);
+
+
+    // has to declare throws Exception because STREAMING declares it
+    public void traverse1() throws Exception {
+        new Routes().stream(STREAMING, false, "ww") // materialize to Traversable<String, Exception>
+            .traverse(t -> t.each(s -> LOG.info(s)));
+    }
+
+    public void traverse2() {
+        new Routes().stream(CATCHING, true, "WW") // materialize to Traversable<String, RuntimeException>
+            .traverse(t -> t.each(s -> LOG.info(s)));
+    }
+}
+```
+
+## Maven configuration
+
+Artifacts can be found on [Maven Central](https://search.maven.org/) after publication.
+
+```xml
+<dependency>
+    <groupId>org.moodminds.routes</groupId>
+    <artifactId>routes-traverse</artifactId>
+    <version>${version}</version>
+</dependency>
+```
+
+## Building from Source
+
+You may need to build from source to use **Routes Traverse** (until it is in Maven Central) with Maven and JDK 9 at least.
+
+## License
+This project is going to be released under version 2.0 of the [Apache License][l].
+
+[l]: https://www.apache.org/licenses/LICENSE-2.0
---
+++ src/main/java/org/moodminds/route/traverse/Routes.java
@@ -0,0 +1,201 @@
+package org.moodminds.route.traverse;
+
+import org.moodminds.route.Emitting;
+import org.moodminds.route.Flow;
+import org.moodminds.route.Flowing;
+import org.moodminds.route.Route;
+import org.moodminds.route.Route1;
+import org.moodminds.route.Route2;
+import org.moodminds.route.Route3;
+import org.moodminds.route.Route4;
+import org.moodminds.route.Route5;
+import org.moodminds.route.Route6;
+import org.moodminds.route.Route7;
+import org.moodminds.route.Route8;
+import org.moodminds.traverse.Traversable;
+
+/**
+ * The materializing {@link Traversable} implementation of the {@link org.moodminds.route.Routes} interface.
+ */
+public class Routes implements org.moodminds.route.Routes {
+
+    @Override
+    public <V, E extends Exception> TraversableEmittable<V, E> stream(Route<? super Flow<?, E>, ? extends Emitting<? extends V>> route) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <V, E extends Exception> TraversableEmittable<V, E> stream(Class<? extends E> thrown, Route<? super Flow<?, E>, ? extends Emitting<? extends V>> route) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <E extends Exception> TraversableEmittable<Void, E> action(Route<? super Flow<?, E>, ? extends Flowing<?>> route) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <E extends Exception> TraversableEmittable<Void, E> action(Class<? extends E> thrown, Route<? super Flow<?, E>, ? extends Flowing<?>> route) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I, V, E extends Exception> TraversableEmittable<V, E> stream(Route1<? super Flow<?, E>, I, ? extends Emitting<? extends V>> route, I value) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I, V, E extends Exception> TraversableEmittable<V, E> stream(Class<? extends E> thrown, Route1<? super Flow<?, E>, I, ? extends Emitting<? extends V>> route, I value) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I, E extends Exception> TraversableEmittable<Void, E> action(Route1<? super Flow<?, E>, I, ? extends Flowing<?>> route, I value) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I, E extends Exception> TraversableEmittable<Void, E> action(Class<? extends E> thrown, Route1<? super Flow<?, E>, I, ? extends Flowing<?>> route, I value) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, V, E extends Exception> TraversableEmittable<V, E> stream(Route2<? super Flow<?, E>, I1, I2, ? extends Emitting<? extends V>> route, I1 value1, I2 value2) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, V, E extends Exception> TraversableEmittable<V, E> stream(Class<? extends E> thrown, Route2<? super Flow<?, E>, I1, I2, ? extends Emitting<? extends V>> route, I1 value1, I2 value2) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, E extends Exception> TraversableEmittable<Void, E> action(Route2<? super Flow<?, E>, I1, I2, ? extends Flowing<?>> route, I1 value1, I2 value2) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, E extends Exception> TraversableEmittable<Void, E> action(Class<? extends E> thrown, Route2<? super Flow<?, E>, I1, I2, ? extends Flowing<?>> route, I1 value1, I2 value2) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, V, E extends Exception> TraversableEmittable<V, E> stream(Route3<? super Flow<?, E>, I1, I2, I3, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, V, E extends Exception> TraversableEmittable<V, E> stream(Class<? extends E> thrown, Route3<? super Flow<?, E>, I1, I2, I3, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, E extends Exception> TraversableEmittable<Void, E> action(Route3<? super Flow<?, E>, I1, I2, I3, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, E extends Exception> TraversableEmittable<Void, E> action(Class<? extends E> thrown, Route3<? super Flow<?, E>, I1, I2, I3, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, V, E extends Exception> TraversableEmittable<V, E> stream(Route4<? super Flow<?, E>, I1, I2, I3, I4, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3, I4 value4) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, V, E extends Exception> TraversableEmittable<V, E> stream(Class<? extends E> thrown, Route4<? super Flow<?, E>, I1, I2, I3, I4, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3, I4 value4) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, E extends Exception> TraversableEmittable<Void, E> action(Route4<? super Flow<?, E>, I1, I2, I3, I4, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3, I4 value4) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, E extends Exception> TraversableEmittable<Void, E> action(Class<? extends E> thrown, Route4<? super Flow<?, E>, I1, I2, I3, I4, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3, I4 value4) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, I5, V, E extends Exception> TraversableEmittable<V, E> stream(Route5<? super Flow<?, E>, I1, I2, I3, I4, I5, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, I5, V, E extends Exception> TraversableEmittable<V, E> stream(Class<? extends E> thrown, Route5<? super Flow<?, E>, I1, I2, I3, I4, I5, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, I5, E extends Exception> TraversableEmittable<Void, E> action(Route5<? super Flow<?, E>, I1, I2, I3, I4, I5, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, I5, E extends Exception> TraversableEmittable<Void, E> action(Class<? extends E> thrown, Route5<? super Flow<?, E>, I1, I2, I3, I4, I5, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, I5, I6, V, E extends Exception> TraversableEmittable<V, E> stream(Route6<? super Flow<?, E>, I1, I2, I3, I4, I5, I6, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, I5, I6, V, E extends Exception> TraversableEmittable<V, E> stream(Class<? extends E> thrown, Route6<? super Flow<?, E>, I1, I2, I3, I4, I5, I6, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, I5, I6, E extends Exception> TraversableEmittable<Void, E> action(Route6<? super Flow<?, E>, I1, I2, I3, I4, I5, I6, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, I5, I6, E extends Exception> TraversableEmittable<Void, E> action(Class<? extends E> thrown, Route6<? super Flow<?, E>, I1, I2, I3, I4, I5, I6, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, I5, I6, I7, V, E extends Exception> TraversableEmittable<V, E> stream(Route7<? super Flow<?, E>, I1, I2, I3, I4, I5, I6, I7, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6, I7 value7) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, I5, I6, I7, V, E extends Exception> TraversableEmittable<V, E> stream(Class<? extends E> thrown, Route7<? super Flow<?, E>, I1, I2, I3, I4, I5, I6, I7, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6, I7 value7) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, I5, I6, I7, E extends Exception> TraversableEmittable<Void, E> action(Route7<? super Flow<?, E>, I1, I2, I3, I4, I5, I6, I7, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6, I7 value7) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, I5, I6, I7, E extends Exception> TraversableEmittable<Void, E> action(Class<? extends E> thrown, Route7<? super Flow<?, E>, I1, I2, I3, I4, I5, I6, I7, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6, I7 value7) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, I5, I6, I7, I8, V, E extends Exception> TraversableEmittable<V, E> stream(Route8<? super Flow<?, E>, I1, I2, I3, I4, I5, I6, I7, I8, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6, I7 value7, I8 value8) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, I5, I6, I7, I8, V, E extends Exception> TraversableEmittable<V, E> stream(Class<? extends E> thrown, Route8<? super Flow<?, E>, I1, I2, I3, I4, I5, I6, I7, I8, ? extends Emitting<? extends V>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6, I7 value7, I8 value8) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, I5, I6, I7, I8, E extends Exception> TraversableEmittable<Void, E> action(Route8<? super Flow<?, E>, I1, I2, I3, I4, I5, I6, I7, I8, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6, I7 value7, I8 value8) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+
+    @Override
+    public <I1, I2, I3, I4, I5, I6, I7, I8, E extends Exception> TraversableEmittable<Void, E> action(Class<? extends E> thrown, Route8<? super Flow<?, E>, I1, I2, I3, I4, I5, I6, I7, I8, ? extends Flowing<?>> route, I1 value1, I2 value2, I3 value3, I4 value4, I5 value5, I6 value6, I7 value7, I8 value8) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+}
